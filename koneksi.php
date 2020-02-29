368d68c30e75c4a1f5488570d4b9505c3ec19adf
rickyjulpiter <rickyjulpiter14@gmail.com>
2020-02-29 11:45:54 +0700

    change koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,22 +1,16 @@
 <?php
 date_default_timezone_set('America/Los_Angeles');
 
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "simasker";
+// $host = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "simasker";
 
 // // Sistem Pintar Server
-// $host = "localhost";
-// $username = "u4502442_teknik";
-// $password = "-v3ns^UG0VbN";
-// $database = "u4502442_teknik";
-
-// // Teknik Server
-// $host = "localhost";
-// $username = "adm_mhs_dbadmin";
-// $password = "ezeen4ushohbu9ai";
-// $database = "adm_mhs_db";
+$host = "localhost";
+$username = "u4502442_simasker";
+$password = "m=eu4Z=QUmwn";
+$database = "u4502442_simasker";
 
 // Koneksi ke MySQL dengan PDO
 $pdo = new PDO('mysql:host=' . $host . ';dbname=' . $database, $username, $password);
